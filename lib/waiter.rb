6241246140aa6f46d906b8064a4ec664c8804127
Paris Dolfman <parisdolfman@gmail.com>
2019-08-15 20:50:43 +0000

    Done.

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -14,4 +14,14 @@ def self.all
   @@all
 end
 
+def new_meal(customer, total, tip = 0)
+  Meal.new(self, customer, total, tip)
 end
+
+def meals
+  Meal.all.select do |meal|
+    meal.waiter == self
+  end
+end
+
+end 
